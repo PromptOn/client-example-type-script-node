7730c01c8bf6a6f726fd5ddb51a9f7ac3f7ee50e
szerintedmi <szerintedmi@gmail.com>
2023-06-15 01:40:18 +0100

    finess test output

--- src/index.ts
+++ src/index.ts
@@ -46,13 +46,17 @@ async function main() {
     try {
         console.log("Getting an inference for a prompt...");
 
-        // TIP: Pass "mock_me_softly" / "timeout_me_softly" / "fail_me_softly"
-        //      in end_user_id for testing
         let inf_result = await prompton.inferences.newInference({
+            // you need to your own prompt and a Live prompt version first
+            // if you pass promptId it picks one from all the live prompt versions randomly
+            //    you can  pass a prompt_version_id instead
             promptId: "6489aea62cf0f680e2d56263",
-            templateArgs: { what: "young" },
-            endUserId: "ppp", // use
-            source: "Node Client Example",
+            templateArgs: { what: "a trump fan" },
+            endUserId: "node client example user: " + username,
+            // endUserId: "mock_me_softly",
+            // endUserId: "timeout_me_softly",
+            // endUserId: "fail_me_softly",
+            clientRefId: "it should work now on staging ",
         });
 
         console.log(`<--- Inference response id: ${inf_result.id}`);
@@ -105,10 +109,23 @@ async function main() {
                 JSON.stringify(inference_error.error)
         );
     } else {
-        let inf_data = inference.response as PromptonApi.InferenceResponseData;
+        let inf_response =
+            inference.response as PromptonApi.InferenceResponseData;
+
         console.log(
-            `  Instructions: ${inference.request?.rawRequest.messages[0].content}\n` +
-                `  Answer: ${inf_data.rawResponse.choices[0].message.content}\n`
+            `Request:\n${inference.request?.rawRequest.messages
+                .map((message) => {
+                    return ` - ${message.role}: ${message.content}\n`;
+                })
+                .join("")}`
+        );
+
+        console.log(
+            `Response:\n${inf_response.rawResponse.choices
+                .map((choice, idx) => {
+                    return ` [choice ${idx}] - ${choice.message.role}: ${choice.message.content}\n`;
+                })
+                .join("")}`
         );
     }
 }
---
+++ src/mock_response.ts
@@ -0,0 +1,110 @@
+import { PromptonApi } from "@prompton/prompton";
+
+let inference: PromptonApi.InferenceRead = {
+    id: "648a518018a9305eac68b549",
+    createdAt: "2023-06-14T23:47:12.677000",
+    createdByUserId: "6489d7bef740a42d34b8e191",
+    createdByOrgId: "6481b9559ec9785efd91cff6",
+    endUserId: "node client example user: peter.petrovics@bodyswaps.co",
+    source: undefined,
+    clientRefId: "it should work now on staging ",
+    templateArgs: {
+        what: "a trump fan",
+    },
+    metadata: undefined,
+    requestTimeout: undefined,
+    promptVersionId: "6489af9f2cf0f680e2d56264",
+    promptVersionIdsConsidered: [],
+    promptId: "6489aea62cf0f680e2d56263",
+    promptVersionName: "test prompt version",
+    status: "Processed",
+    request: {
+        provider: "OpenAI",
+        rawRequest: {
+            model: "gpt-3.5-turbo",
+            temperature: 1,
+            topP: undefined,
+            stop: undefined,
+            maxTokens: 500,
+            presencePenalty: undefined,
+            frequencyPenalty: undefined,
+            logitBias: undefined,
+            messages: [
+                {
+                    role: "system",
+                    content:
+                        "You are a tired old man. Answer all questions with maximum 40 words",
+                    name: undefined,
+                },
+                {
+                    role: "user",
+                    content: "Tell me, am I a trump fan",
+                    name: undefined,
+                },
+            ],
+            n: 1,
+            stream: false,
+            user: "node client example user: peter.petrovics@bodyswaps.co",
+        },
+    },
+    response: {
+        completedAt: "2023-06-14T23:50:54.920354",
+        completitionDurationSeconds: 1.2696,
+        isClientConnectedAtFinish: true,
+        isError: false,
+        tokenUsage: {
+            promptTokens: 36,
+            completionTokens: 11,
+            totalTokens: 47,
+        },
+        rawResponse: {
+            id: "chatcmpl-7RUUj2VaYwabEBFBQdfj2sG4sINuV",
+            object: "chat.completion",
+            created: 1686786433,
+            model: "gpt-3.5-turbo-0301",
+            usage: {
+                promptTokens: 36,
+                completionTokens: 11,
+                totalTokens: 47,
+            },
+            choices: [
+                {
+                    message: {
+                        role: "assistant",
+                        content: "I do not know if you are a Trump fan.",
+                        name: undefined,
+                    },
+                    finishReason: "stop",
+                    index: 0,
+                },
+                {
+                    message: {
+                        role: "assistant",
+                        content: "2I do not know if you are a Trump fan.",
+                        name: undefined,
+                    },
+                    finishReason: "stop",
+                    index: 0,
+                },
+            ],
+        },
+    },
+};
+
+let inf_response = inference.response as PromptonApi.InferenceResponseData;
+
+console.log(
+    `Request:\n${inference.request?.rawRequest.messages
+        .map((message) => {
+            return ` - ${message.role}: ${message.content}\n`;
+        })
+        .join("")}`
+);
+
+console.log(
+    `Response:\n${inf_response.rawResponse.choices
+        .map((choice, idx) => {
+            return ` [choice ${idx}] - ${choice.message.role}: ${choice.message.content}\n`;
+        })
+        .join("")}`
+);
